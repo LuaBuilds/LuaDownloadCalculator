d9912d29b40e00ba06694329bad12a7da9596bd1
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2021-12-03 08:26:38 -0500

    Create embed.js

---
+++ htmlembed/embed.js
@@ -0,0 +1,6 @@
+function findlua()
+{
+    var vers = document.getElementById("vers").value
+   var myWindow1 = window.open("https://github.com/LuaBuilds/Windows-Builds/blob/main/Lua_"+ vers + ".zip?raw=true", '', 'scrollbars=1,height='+screen.availHeight+',width='+screen.availWidth);
+}
+  
